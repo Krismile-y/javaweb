b63dce98656498a3bee5828a43028013f1302940
yuanhang <2587216169@qq.com>
2021-05-21 10:32:56 +0800

    更改了配置文件

---
+++ src/views/home.vue
@@ -0,0 +1,24 @@
+<template>
+<div>
+
+</div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+name: "home",
+  created() {
+  axios({
+    url:'http://localhost:3000/home'
+  }).then(res=>{
+    console.log(res);
+  })
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
